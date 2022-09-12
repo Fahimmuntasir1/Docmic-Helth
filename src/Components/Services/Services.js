1cc89a78b47ecae7097fe496808ac8fca0c966e5
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2022-09-12 21:11:36 +0100

    sservice section done

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -3,8 +3,52 @@ import React from "react";
 const Services = () => {
   return (
     <div>
-      <p className="text-[16px]">Our Services</p>
-      <h2 className="text-[32px]">Services For Your Health</h2>
+      <div className="text-center">
+        <p className="text-[16px] text-[#642DFF] font-medium">Our Services</p>
+        <h2 className="text-[32px] text-[#1E1E1E]">Services For Your Health</h2>
+      </div>
+      <div className="lg:flex mt-[28px] gap-[21px] justify-center">
+        <div className="w-[236px] h-[152px] border p-[12px]">
+          <div className="flex justify-between mb-[12px]">
+            <h3 className="text-[12px] text-[#1E1E1E]">Cardiology</h3>
+            <img src="" alt="" />
+          </div>
+          <p className="text-[14px] text-[#575757] mb-[24px]">
+            Seduahag perspiciati under omnised atused error.
+          </p>
+          <button className="text-[16px] text-[#1E1E1E]">Explore Now</button>
+        </div>
+        <div className="w-[236px] h-[152px] border p-[12px]">
+          <div className="flex justify-between mb-[12px]">
+            <h3 className="text-[12px] text-[#1E1E1E]">Monthly Check Up</h3>
+            <img src="" alt="" />
+          </div>
+          <p className="text-[14px] text-[#575757] mb-[24px]">
+            Seduahag perspiciati under omnised atused error.
+          </p>
+          <button className="text-[16px] text-[#1E1E1E]">Explore Now</button>
+        </div>
+        <div className="w-[236px] h-[152px] border p-[12px]">
+          <div className="flex justify-between mb-[12px]">
+            <h3 className="text-[12px] text-[#1E1E1E]">Dental Care</h3>
+            <img src="" alt="" />
+          </div>
+          <p className="text-[14px] text-[#575757] mb-[24px]">
+            Seduahag perspiciati under omnised atused error.
+          </p>
+          <button className="text-[16px] text-[#1E1E1E]">Explore Now</button>
+        </div>
+        <div className="w-[236px] h-[152px] border p-[12px]">
+          <div className="flex justify-between mb-[12px]">
+            <h3 className="text-[12px] text-[#1E1E1E]">Ophthalmology</h3>
+            <img src="" alt="" />
+          </div>
+          <p className="text-[14px] text-[#575757] mb-[24px]">
+            Seduahag perspiciati under omnised atused error.
+          </p>
+          <button className="text-[16px] text-[#1E1E1E]">Explore Now</button>
+        </div>
+      </div>
     </div>
   );
 };
