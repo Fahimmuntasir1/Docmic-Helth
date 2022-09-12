4b39dec897eae293c71c640c41534d94809d3e81
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2022-09-12 19:53:42 +0100

    service section running

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./Components/Navbar/Navbar";
+import Services from "./Components/Services/Services";
 
 function App() {
   return (
     <div className="w-11/12 mx-auto">
       <Navbar />
+      <Services />
     </div>
   );
 }
---
+++ src/Components/Services/Services.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div>
+      <p className="text-[16px]">Our Services</p>
+      <h2 className="text-[32px]">Services For Your Health</h2>
+    </div>
+  );
+};
+
+export default Services;
